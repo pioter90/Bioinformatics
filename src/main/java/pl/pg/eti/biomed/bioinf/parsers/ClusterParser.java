552656f8935438d65556dac68ef189aa17cd52c4
ifar <ifarrafi@gmail.com>
2014-12-13 17:10:32 +0100

    Add Parser ArrayLists

--- src/main/java/pl/pg/eti/biomed/bioinf/parsers/ClusterParser.java
+++ src/main/java/pl/pg/eti/biomed/bioinf/parsers/ClusterParser.java
@@ -1,5 +1,42 @@
 package pl.pg.eti.biomed.bioinf.parsers;
+import pl.pg.eti.biomed.bioinf.dao.Edge;
+import pl.pg.eti.biomed.bioinf.dao.Tree;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class ClusterParser {
-   //TO DO !
+
+    public static Tree parseFile(String filePath) throws IOException {
+        ArrayList<Edge> edges = new ArrayList<Edge>();
+
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(filePath));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+            }
+            String everything = sb.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            br.close();
+        }
+
+        return null;
+    }
+
+
 }
