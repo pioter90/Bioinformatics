d5a6a03e4bc7f4f9a359bd6b21dcbd236773453e
ifar <ifarrafi@gmail.com>
2014-12-14 21:58:22 +0100

    Add to Consensus Test Tree with Major Consensus

--- test/pl/pg/eti/biomed/bioinf/algorithms/ConsensusTreesTests.java
+++ test/pl/pg/eti/biomed/bioinf/algorithms/ConsensusTreesTests.java
@@ -10,16 +10,19 @@ import java.util.ArrayList;
 
 public class ConsensusTreesTests {
     private ArrayList<Tree> firstTree;
-    private Tree firstConsensusTree;
+    private Tree firstConsensusTreeStrictConsensus;
+    private Tree firstConsensusTreeMajorConsensus;
+
 
     @Before
     public void setup(){
         firstTree = ClusterParser.getTreesFromFile("Tests/MultipleTreesTests/ConsensusTrees_Sample1.txt", "####");
-        firstConsensusTree = ConsensusTreeBuilder.getConsensusTree(firstTree, 100);
+        firstConsensusTreeStrictConsensus = ConsensusTreeBuilder.getConsensusTree(firstTree, 100);
+        firstConsensusTreeMajorConsensus = ConsensusTreeBuilder.getConsensusTree(firstTree, 50);
     }
     @Test
-    public void checkFirstNode(){
-        Edge first = firstConsensusTree.getEdgeAt(0);
+    public void checkFirstNodeStrictConsensus(){
+        Edge first = firstConsensusTreeStrictConsensus.getEdgeAt(0);
         ArrayList<Leaf> leaves = first.getLeaves();
         Assert.assertEquals(5,leaves.size());
         Assert.assertEquals("A", leaves.get(0).getId());
@@ -29,31 +32,46 @@ public class ConsensusTreesTests {
         Assert.assertEquals("E", leaves.get(4).getId());
     }
     @Test
-    public void checkSecondNode(){
-        Edge second = firstConsensusTree.getEdgeAt(1);
+    public void checkFirstNodeMajorConsensus(){
+        Edge first = firstConsensusTreeMajorConsensus.getEdgeAt(0);
+        ArrayList<Leaf> leaves = first.getLeaves();
+        Assert.assertEquals(5,leaves.size());
+        Assert.assertEquals("A", leaves.get(0).getId());
+        Assert.assertEquals("B", leaves.get(1).getId());
+        Assert.assertEquals("C", leaves.get(2).getId());
+        Assert.assertEquals("D", leaves.get(3).getId());
+        Assert.assertEquals("E", leaves.get(4).getId());
+    }
+    @Test
+     public void checkSecondNodeStrictConsensus(){
+        Edge second = firstConsensusTreeStrictConsensus.getEdgeAt(1);
         ArrayList<Leaf> leaves = second.getLeaves();
-        Assert.assertTrue(leaves.size()==1);
-        Assert.assertEquals(leaves.get(0).getId(), "A");
+        Assert.assertEquals(1, leaves.size());
+        Assert.assertEquals("A", leaves.get(0).getId());
 
     }
     @Test
-    public void checkThirdNode(){
-        Edge third = firstConsensusTree.getEdgeAt(2);
+    public void checkSecondNodeMajorConsensus(){
+        Edge second = firstConsensusTreeMajorConsensus.getEdgeAt(1);
+        ArrayList<Leaf> leaves = second.getLeaves();
+        Assert.assertEquals(2, leaves.size());
+        Assert.assertEquals("C", leaves.get(0).getId());
+        Assert.assertEquals("D", leaves.get(1).getId());
+
+    }
+    @Test
+    public void checkThirdNodeStrictConsensus(){
+        Edge third = firstConsensusTreeStrictConsensus.getEdgeAt(2);
         ArrayList<Leaf> leaves = third.getLeaves();
-        Assert.assertTrue(leaves.size()==1);
-        Assert.assertEquals(leaves.get(0).getId(), "B");
+        Assert.assertEquals(1, leaves.size());
+        Assert.assertEquals("B", leaves.get(0).getId());
 
     }
     @Test
-    public void displayTrees(){
-        ConsensusTreeBuilder.getConsensusTree(firstTree,100);
-
-        //for(int i=0; i<2; i++){
-            //System.out.println("Tree number "+i);
-            //System.out.println(ConsensusTreeBuilder.getConsensusTree(firstTree, 100).getTreeVisualisation());
-        System.out.println(ConsensusTreeBuilder.getConsensusTree(firstTree, 100).getTreeVisualisation());
-            System.out.println("======================");
-       // }
-
+    public void checkThirdNodeMajorConsensus(){
+        Edge third = firstConsensusTreeMajorConsensus.getEdgeAt(2);
+        ArrayList<Leaf> leaves = third.getLeaves();
+        Assert.assertEquals(1, leaves.size());
+        Assert.assertEquals("A", leaves.get(0).getId());
     }
 }
