a1b49dd27c91b1f07033c5c7a8d2c7dc6dcdb9bf
Piotrek <Piotrek@dell.local>
2014-12-14 16:44:56 +0100

    Added three trees test

--- test/pl/pg/eti/biomed/bioinf/parsers/MultipleTreesReadTest.java
+++ test/pl/pg/eti/biomed/bioinf/parsers/MultipleTreesReadTest.java
@@ -9,35 +9,48 @@ import pl.pg.eti.biomed.bioinf.dao.Tree;
 
 
 public class MultipleTreesReadTest {
-	private ArrayList<Tree> trees;
+	private ArrayList<Tree> twoTrees;
+	private ArrayList<Tree> threeTrees;
 	
 	@Before
 	public void setup(){
-		trees = ClusterParser.getTreesFromFile("Tests/MultipleTreesTests/MultipleTreesRead_Sample1.txt", "####");
+		twoTrees = ClusterParser.getTreesFromFile("Tests/MultipleTreesTests/MultipleTreesRead_Sample1.txt", "####");
+		threeTrees  = ClusterParser.getTreesFromFile("Tests/MultipleTreesTests/MultipleTreesRead_Sample2.txt", "####");
 	}
 	
 	@Test
 	public void testNumberOfLoadedTrees(){
-		Assert.assertEquals(2, trees.size());
+		Assert.assertEquals(2, twoTrees.size());
+		Assert.assertEquals(3, threeTrees.size());
 	}
 	
 	@Test
 	public void testNumberOfEdgesInFirstTree(){
-		Tree first = trees.get(0);
+		Tree first = twoTrees.get(0);
+		Assert.assertEquals(8, first.getNumberOfEdges());
+		first = threeTrees.get(0);
 		Assert.assertEquals(8, first.getNumberOfEdges());
 	}
 	
 	@Test
 	public void testNumberOfEdgesInSecondTree(){
-		Tree second = trees.get(1);
+		Tree second = twoTrees.get(1);
 		Assert.assertEquals(7, second.getNumberOfEdges());
+		second = threeTrees.get(1);
+		Assert.assertEquals(6, second.getNumberOfEdges());
+	}
+	
+	@Test
+	public void testNumberOfEdgesInThirdTree(){
+		Tree third = threeTrees.get(2);
+		Assert.assertEquals(10, third.getNumberOfEdges());
 	}
 	
 	@Test
 	public void displayTrees(){
 		for(int i=0; i<2; i++){
 			System.out.println("Tree number "+i);
-			System.out.println(trees.get(i).getTreeVisualisation());
+			System.out.println(twoTrees.get(i).getTreeVisualisation());
 			System.out.println("======================");
 		}
 		
